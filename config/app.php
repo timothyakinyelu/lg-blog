4b50a5e8c0d3f4de2170208ecfe462581a2557b0
loverofMush <tolujamal1@gmail.com>
2020-01-05 00:45:10 +0100

    app url change

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://larissagideon.herokuapp.com'),
+    'url' => env('APP_URL', 'http://www.larissagideon.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
