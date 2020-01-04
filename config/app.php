c3a0c06e0bdc4000ff992ad09acd9cda2858606f
loverofMush <tolujamal1@gmail.com>
2020-01-05 00:10:37 +0100

    config/app changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Larissa Gideon'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://larissagideon.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
