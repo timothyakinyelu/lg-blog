5f635a17eb174b36c077f14776f95c58c8a3b0bd
loverofMush <tolujamal1@gmail.com>
2020-01-05 17:50:29 +0100

    images

--- app/Category.php
+++ app/Category.php
@@ -12,25 +12,6 @@ class Category extends Model
         return $this->hasMany('App\Post');
     }
 
-    // //Photos relationship
-    // public function photos() 
-    // {
-    //     return $this->hasManyThrough('App\Photo', 'App\Post');
-    // }
-
-    // //Tags relationship
-    // public function tags() 
-    // {
-    //     return $this->hasManyThrough(
-    //         'App\Tag', // The model to access to
-    //         'App\PostTag', // The intermediate table that connects the User with the Podcast.
-    //         'tag_id',
-    //         'post_id'
-    //         // 'post_id',
-    //         // 'tag_id',
-    //     );
-    // }
-
     // //Comments relationship
     // public function comments() 
     // {
